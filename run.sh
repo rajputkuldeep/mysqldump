40783af482d92b2d4ffc5ee68d6c771a92fc459e
Kuldeep Rajput <kuldeep.rajput@300minds.com>
2022-09-01 14:19:25 +0530

    sh file changes

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 echo "Dumping database"
 USER="root"
-PASSWORD="root"
+PASSWORD="password"
 
 databases=`mysql -u $USER -p$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
 
 for db in $databases; do
+    echo "In loop"
     if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] ; then
         echo "Dumping database: $db"
         mysqldump -u $USER -p$PASSWORD --databases $db > `date +%Y%m%d`.$db.sql
