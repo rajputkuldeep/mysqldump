10fd0310bff0dbd312f3ce7945947b221b7be895
Kuldeep Rajput <kuldeep.rajput@300minds.com>
2022-09-01 15:54:14 +0530

    commit

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const mysqldump = require('mysqldump')
 // cron.schedule('* * * * *', async () => {
 console.log("Enter in cron")
 
-exec('mysqldump --user=root --password=Mind@1234 --host=13.235.179.4 --all-databases > ./zip/dump.sql', async (error, stderr) => {
+exec('sudo mysql -u root -pMind@1234 -h13.235.179.4 -e "SHOW DATABASES;" | tr -d "| " | grep -v Database', async (error, stderr, stdout) => {
+    console.log(stdout, "stdout")
     if (error) {
         console.log("mysqlDump error: ", error)
     }
---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Dumping database"
+USER="root"
+PASSWORD="password"
+
+databases=`mysql -u $USER -p$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
+
+for db in $databases; do
+    echo "$db"
+    if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] ; then
+        echo "Dumping database: $db"
+        mysqldump -u $USER -p$PASSWORD --databases $db > `date +%Y%m%d`.$db.sql
+       # gzip $OUTPUT/`date +%Y%m%d`.$db.sql
+    fi
+done
